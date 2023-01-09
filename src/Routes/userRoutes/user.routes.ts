24e8372ac3077dc4c80325dac53f50f937b40c35
Matheus Felipe <matheusfelipetp@outlook.com>
2023-01-09 14:07:52 -0300

    fix: routes

--- src/Routes/userRoutes/user.routes.ts
+++ src/Routes/userRoutes/user.routes.ts
@@ -3,9 +3,12 @@ import { Router } from "express";
 import { UserController } from "../../Controllers/UserController";
 import { UserMiddleware } from "../../Middlewares/userMiddleware";
 
+const userMiddleware = new UserMiddleware();
+const userController = new UserController();
+
 export const userRoutes = Router();
 
-userRoutes.post("/", new UserMiddleware().emailExists, new UserController().create);
+userRoutes.post("/", userMiddleware.emailExists, userController.create);
 userRoutes.get("");
 userRoutes.get("/:id");
 userRoutes.patch("/:id");
