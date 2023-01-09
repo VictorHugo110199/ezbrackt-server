7b0bc33f0f0493380466c314171024922cc97e82
Matheus Felipe <matheusfelipetp@outlook.com>
2023-01-09 12:40:14 -0300

    feat: add user is active middleware

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 
-import { createUserInterface } from "../interfaces/userInterfaces/userInterface";
+import { ICreateUser } from "../interfaces/userInterfaces/userInterface";
 import { UserService } from "../Services/UserService";
 
 export class UserController {
   async create(req: Request, res: Response) {
-    const payload: createUserInterface = req.body;
+    const payload: ICreateUser = req.body;
 
     const user = await new UserService().create(payload);
 
