fd6779c49f532963e7456b493a3bb56e0499b3bb
Matheus Felipe <matheusfelipetp@outlook.com>
2023-01-10 13:49:36 -0300

    fix: create competition services

--- src/services/Competitions.service.ts
+++ src/services/Competitions.service.ts
@@ -1,15 +1,17 @@
+import Competitions from "../entities/Competitions.entity";
 import { ICreateCompetition } from "../interfaces/competition.interface";
 import { competitionRepository } from "../repositories/competition.repository";
 
 export class CompetitionService {
-  async create(payload: ICreateCompetition, userId: string) {
+  async create(payload: ICreateCompetition, userId: string): Promise<Competitions> {
     const { name, number_players, description } = payload;
 
     const newCompetition = competitionRepository.create({
       name,
       number_players,
       description,
-      userId
+      userId,
+      players: []
     });
 
     await competitionRepository.save(newCompetition);
