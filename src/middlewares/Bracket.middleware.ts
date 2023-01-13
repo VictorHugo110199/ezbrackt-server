060890d6bc5bfa039b39eeb0c6395fa79e0cd56d
Matheus Felipe <matheusfelipetp@outlook.com>
2023-01-13 13:25:12 -0300

    fix: resolved conflicts

--- src/middlewares/Bracket.middleware.ts
+++ src/middlewares/Bracket.middleware.ts
@@ -14,4 +14,28 @@ export class BracketMiddleware {
 
     next();
   }
+
+  async ensurePlayerExistsOnBrackte(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const idBrackets = req.params.idBrackets;
+    const { winner } = req.body;
+
+    const bracket = await bracketRepository.findOne({
+      where: {
+        id: idBrackets
+      },
+      relations: {
+        player1: true,
+        player2: true,
+        winner: true,
+        loser: true,
+        competition: true
+      }
+    });
+
+    if (winner !== bracket?.player1.id && bracket?.player2.id !== bracket?.player1.id && winner) {
+      throw new NotFoundError("Player Não encontrado!");
+    }
+
+    next();
+  }
 }
--- src/routes/bracket.routes.ts
+++ src/routes/bracket.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 
 import { BracketsController } from "../controllers/Brackets.controller";
-import { BracktesMiddleware } from "../middlewares/Brackets.middleware";
 import { BracketMiddleware } from "../middlewares/Bracket.middleware";
 import { CompetitionMiddleware } from "../middlewares/Competition.middleware";
 import { UserMiddleware } from "../middlewares/User.middleware";
@@ -9,16 +8,7 @@ import { UserMiddleware } from "../middlewares/User.middleware";
 export const bracketsRouter = Router();
 
 const bracketsController = new BracketsController();
-const bracktesMiddleware = new BracktesMiddleware();
-
-bracketsRouter.post("/init/:id", bracketsController.create);
-bracketsRouter.post(
-  "/winner/:idBrackets",
-  bracktesMiddleware.ensurePlayerExistsOnBrackte,
-  bracketsController.winnerPlayer
-);
-
-const bracketMiddleware = new BracketMiddleware();
+const bracktesMiddleware = new BracketMiddleware();
 const userMiddleware = new UserMiddleware();
 const competitionMiddleware = new CompetitionMiddleware();
 
@@ -33,7 +23,8 @@ bracketsRouter.post(
 bracketsRouter.post(
   "/winner/:idBrackets",
   userMiddleware.tokenExists,
-  bracketMiddleware.idExists,
+  bracktesMiddleware.idExists,
+  bracktesMiddleware.ensurePlayerExistsOnBrackte,
   bracketsController.winnerPlayer
 );
 
