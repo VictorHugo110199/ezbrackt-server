1a5c62c4d64fdf0d70825ba32bad24cd9a26b305
VictorHugo110199 <victorhugo110199@gmail.com>
2023-01-13 13:10:17 -0300

    Feat: Middleware Exists Player

--- src/routes/bracket.routes.ts
+++ src/routes/bracket.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 
 import { BracketsController } from "../controllers/Brackets.controller";
+import { BracktesMiddleware } from "../middlewares/Brackets.middleware";
 
 export const bracketsRouter = Router();
 
 const bracketsController = new BracketsController();
+const bracktesMiddleware = new BracktesMiddleware();
 
 bracketsRouter.post("/init/:id", bracketsController.create);
-bracketsRouter.post("/winner/:idBrackets", bracketsController.winnerPlayer);
+bracketsRouter.post(
+  "/winner/:idBrackets",
+  bracktesMiddleware.ensurePlayerExistsOnBrackte,
+  bracketsController.winnerPlayer
+);
 bracketsRouter.post("/createNew/:idCompetition", bracketsController.createNew);
