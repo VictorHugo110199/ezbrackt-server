3a9e244f57c8b8f51a866564960bfbd810cf2187
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2023-01-16 17:07:38 -0300

    feat: add function to user received photo

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,7 +2,10 @@ import { Router } from "express";
 
 import { CompetitionController } from "../controllers/Competition.controller";
 import { UserController } from "../controllers/User.controller";
+import User from "../entities/User.entity";
+import { cloudinaryFunction, uploadImage } from "../middlewares/photo.middleware";
 import { UserMiddleware } from "../middlewares/User.middleware";
+import { userRepository } from "../repositories/user.repository";
 
 const userMiddleware = new UserMiddleware();
 const userController = new UserController();
@@ -10,7 +13,19 @@ const competitionController = new CompetitionController();
 
 export const userRoutes = Router();
 
-userRoutes.post("/", userMiddleware.emailExists, userController.create);
+userRoutes.get("/teste/:id", async (req, res) => {
+  const { id } = req.params;
+  const teste = await userRepository
+    .createQueryBuilder()
+    .select("users.photo")
+    .from(User, "users")
+    .where("users.id = :id", { id })
+    .getOne();
+
+  return res.send(teste);
+});
+
+userRoutes.post("/", uploadImage, cloudinaryFunction, userMiddleware.emailExists, userController.create);
 
 userRoutes.get("/", userMiddleware.tokenExists, userController.getUsers);
 
